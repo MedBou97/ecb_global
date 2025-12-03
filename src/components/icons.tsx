02cd8bc8aa3e7e126e9b989b4c9a1d8bfa2ddacf
Ali Legha <alilegha50@gmail.com>
2025-12-03 21:19:14 +0000

    change from svg to png how

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function EcbLogo() {
   return (
     <Image
-      src="/images/logo.svg"
+      src="/images/logo.png"
       alt="ECB Global Logo"
       width={140}
       height={56}
